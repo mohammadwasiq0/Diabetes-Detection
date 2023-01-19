90f03cc1b5126d8665c63c9cd764a9766263a6c8
Mohammad Wasiq <118173141+mohammadwasiq0@users.noreply.github.com>
2023-01-20 00:25:10 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# Diabetic-Project
-Streamlit
+# Diabetes Predictor
+Predict Diabetes using Machine Learning.
+
+In this project, our objective is to predict whether the patient has diabetes or not based on various features like Glucose level, Insulin, Age, BMI and various other features.  We will perform all the steps from Data gathering to Model deployment. During Model evaluation, we compare various machine learning algorithms on the basis of accuracy_score metric and find the best one. Then we create a web app using streamlit. 
+
+Check out the live Demo of the App: [WebApp](https://diabetes-predictionmodel-tarun.herokuapp.com/)
+Read more about it in my [Blogpost](https://medium.com/@ktarun1681/diabetes-prediction-web-app-ea49147f6ed0).
+
+![image](https://user-images.githubusercontent.com/92631457/152490814-91d09a0a-47ad-40af-8270-82756f4db4e4.png)
+
+
+Diabetes is an increasingly growing health issue due to our inactive lifestyle. If it is detected in time then through proper medical treatment, adverse effects can be prevented. To help in early detection, technology can be used very reliably and efficiently. Using machine learning we have built a predictive model that can predict whether the patient is diabetes positive or not.
---
+++ app.py
@@ -0,0 +1,89 @@
+
+
+import numpy as np
+import pickle
+import streamlit as st
+
+#loading the saved model
+
+loaded_model = pickle.load(open('diabetesmodel.pkl', 'rb'))
+
+
+#creating a function for prediction
+
+def diabetes_prediction(input_data):
+
+    # changing the input_data to numpy array
+    input_data_as_numpy_array = np.asarray(input_data)
+
+    # reshape the array as we are predicting for one instance
+    input_data_reshaped = input_data_as_numpy_array.reshape(1,-1)
+
+
+
+
+
+    prediction = loaded_model.predict(input_data_reshaped)
+
+
+    if (prediction[0] == 0):
+      return('The person is not diabetic.')
+    else:
+      return('The person is diabetic.')
+  
+def main():
+    #give a title
+    st.title('Diabetes Prediction Web App')
+    
+    							
+    
+    #getting the input from the user
+    
+    Pregnancies = st.text_input('No. of Pregnancies')
+    Glucose = st.text_input('Blood Glucose Level')
+    BloodPressure = st.text_input('Blood Pressure')
+    SkinThickness = st.text_input('Skin Thickness')
+    Insulin = st.text_input('Insulin Level')
+    BMI = st.text_input('BMI')
+    DiabetesPedigreeFunction = st.text_input('Diabetes Pedigree Function Value')
+    Age = st.text_input('Age of the Person')
+    
+    #code for prediction
+    diagnosis = ''
+    
+    #creating a button for prediction
+    
+    if st.button('Diabetes Test Result'):
+        diagnosis = diabetes_prediction([Pregnancies, Glucose, BloodPressure,SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age ])
+    
+    st.success(diagnosis)
+    
+    
+    
+if __name__ == '__main__':
+    main()
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
